3516a9155bdf3aeb7fd4e1e95cd7ef958268af41
Joy <jinkyung419@gmail.com>
2023-02-21 10:42:32 +0900

    add create

---
+++ src/pages/create.js
@@ -0,0 +1,32 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+export default function Create() {
+    return (
+        <>
+            <h1>
+                <a href="/">WEB</a>
+            </h1>
+            <ol>
+                <li>
+                    <a href="/read/1">html</a>
+                </li>
+                <li>
+                    <a href="/read/2">css</a>
+                </li>
+            </ol>
+            <article>
+                <h2>Welcome</h2>Hello, WEB 2!!
+            </article>
+            <ul>
+                <li>
+                    <a href="/create">Create</a>
+                </li>
+                <li>
+                    <a href="/update">Update</a>
+                </li>
+                <li>
+                    <a href="/delete">Delete</a>
+                </li>
+            </ul>
+        </>
+    );
+}
